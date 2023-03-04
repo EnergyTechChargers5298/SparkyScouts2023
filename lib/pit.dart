9ced0f394505042cc19b06bb6ecb9ed445a0dc2a
Marcus Coppa <marcuscoppa7@gmail.com>
2023-03-04 17:11:51 -0500

    Temp save

---
+++ lib/pit.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:sparky_scouts/main.dart';
+import 'package:sparky_scouts/match.dart';
+import 'package:sparky_scouts/sync.dart';
+
+class PitPage extends StatelessWidget {
+  const PitPage({Key? key}) : super(key: key);
+
+  @override
+  //from here
+  Widget build(BuildContext context) {
+    return Row(
+      children: const <Widget>[],
+    );
+  }
+}
