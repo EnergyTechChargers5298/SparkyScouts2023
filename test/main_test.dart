738f9371629c1006d11ad2733c207fe7207b81c3
Tqkoyaki <np768@scarletmail.rutgers.edu>
2023-03-06 16:03:43 -0500

    Fake Test?

--- test/main_test.dart
+++ test/main_test.dart
@@ -3,6 +3,10 @@
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// tree, read text, and verify that the values of widget properties are correct.index.webp
 
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Fake Test?', (WidgetTester tester) async {});
+}
