d99cb39a55fdfa2d507768d873bb80818dd475d7
Marcus Coppa <marcuscoppa7@gmail.com>
2023-03-14 19:48:45 -0400

    marcus proofed

--- lib/screens/match/extra.dart
+++ lib/screens/match/extra.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sparky_scouts/models/match.dart';
 
+import '../../main.dart';
+
 class MatchExtraPage extends StatefulWidget {
   const MatchExtraPage({super.key});
 
@@ -12,9 +14,14 @@ class _MatchExtraPageState extends State<MatchExtraPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        MatchSurvey.instance.sendData();
-      }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          MatchSurvey.instance.sendData();
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => const RootPage()));
+        },
+        child: const Icon(Icons.upload),
+      ),
     );
   }
 }
